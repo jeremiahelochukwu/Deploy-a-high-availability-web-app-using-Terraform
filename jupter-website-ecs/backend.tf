984b522d5c9645314d45cde527fce4cf04100228
Jeremiah-Ani <jeremiahelochukwu@gmail.com>
2023-08-26 04:01:54 +1400

    created and updated a backend state file and its variable .tf file

---
+++ jupter-website-ecs/backend.tf
@@ -0,0 +1,9 @@
+# store the state terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-bucket-project-a1"
+    key = "jupiter-website-ecs.tfstate"
+    region = "us-east-1"
+    profile = "terraform-online-course"
+  }
+} 
